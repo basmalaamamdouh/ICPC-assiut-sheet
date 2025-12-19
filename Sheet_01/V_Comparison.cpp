493ff5f3c89dcb74e0c1664e47a3eb0514306e76
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2025-12-19 13:01:41 +0200

    Implement comparison logic in V_Comparison.cpp

---
+++ Sheet_01/V_Comparison.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include<iomanip>
+using namespace std;
+ 
+int main()
+{
+ 
+int a, b;
+char s;
+cin >> a >> s >> b;
+if (s == '>' && a > b || s == '<' && a < b || s == '=' && a == b)
+{
+    cout << "Right";
+}
+else
+cout << "Wrong";       
+   
+}
+ 
