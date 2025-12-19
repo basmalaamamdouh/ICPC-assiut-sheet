d2310981f1368db2614c6f9984342a6f24dee568
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2025-12-19 12:58:42 +0200

    Implement basic calculator in O_Calculator.cpp

---
+++ Sheet_01/O_Calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+   int a, b;char s;
+    cin >> a >> s >> b;
+    if (s == '+')
+    {
+        cout << a + b;
+    }
+    else if (s == '-')
+    {
+        cout << (a - b);
+    }
+    else if (s == '*')
+    {
+        cout << a * b;
+    }
+    else
+        cout << (a / b);
+}
