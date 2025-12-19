763ddfdd9cc4e5aa73cccced15a6555e18dbbd1f
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2025-12-19 12:41:52 +0200

    Add Area_of_a_Circle.cpp to calculate circle area

---
+++ Sheet_01/Area_of_a_Circle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include<iomanip>
+using namespace std;
+ 
+int main()
+{
+    cout << fixed << setprecision(9);
+   const double pi  = 3.141592653;
+   double a;
+   cin >> a;
+   double area = a *a* pi;
+   cout << area;
+ 
+}
+ 
+ 
+   
