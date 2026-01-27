3c189d7c5eab4a5535b82be5142dbe3908eb2955
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-27 20:03:36 +0200

    Add program to sum consecutive odd numbers between X and Y

---
+++ sheet_02/S-Sum of Consecutive Odd Numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int T;
+    cin >> T;
+ 
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+ 
+        // Make sure X is smaller than Y
+        if (X > Y) {
+            swap(X, Y);
+        }
+ 
+        int sum = 0;
+ 
+        // Calculate the sum of odd numbers between X and Y (excluding X and Y)
+        for (int i = X + 1; i < Y; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+ 
+        cout << sum << endl;
+    }
+ 
+    return 0;
+}
