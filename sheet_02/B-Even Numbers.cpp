9568700485ab2f5ab33877b7f7686fe410a80f42
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-27 19:48:16 +0200

    Create B-Even Numbers.cpp

---
+++ sheet_02/B-Even Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int n = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0) 
+		{
+			cout << i << endl;
+		}
+		
+	}
+	
+	if (n == 1)
+	{
+		cout << "-1";
+	}
+	
+ 
+ 
+}
