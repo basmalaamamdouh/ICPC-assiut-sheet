d56928bf76e683c931662e85da8f6d8f70f41d49
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-27 20:04:53 +0200

    Create J-Primes from 1 to n.cpp

---
+++ sheet_02/J-Primes from 1 to n.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+    int N;
+    cin >> N;
+ 
+    for (int i = 2; i <= N; i++) {
+        bool isPrime = true;
+        for (int j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
