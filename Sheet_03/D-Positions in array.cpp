2dc87b64e070a1921e0e3a1cfaa182195187c558
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-28 14:10:26 +0200

    Add program to display positions of elements in array
    Implement a program to read an array and print elements less than or equal to 10.

---
+++ Sheet_03/D-Positions in array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() 
+{
+    long long size ;
+    cin >> size;
+    vector<long long> arr(size);
+  
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+       
+        
+    }
+      
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]<=10)
+       cout<<"A["<<i<<"] = " <<arr[i] << endl;
+ 
+    }
+   
+   
+    
+}
