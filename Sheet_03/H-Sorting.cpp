e47a345e3e9f5d1c7d30a8393f042000b04fa20b
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-28 14:13:15 +0200

    Add bubble sort implementation in H-Sorting.cpp
    Implement bubble sort algorithm to sort an array.

---
+++ Sheet_03/H-Sorting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main()
+{
+    long long size;
+    cin >> size;
+    bool symm = true;
+    vector<long long> arr(size);
+ 
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < size; i++) 
+    {
+        for (int j = 0; j < size-i-1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+ 
+        }
+    }
+    for (int i = 0; i < size; i++)
+        cout << arr[i]<<" ";
+}
