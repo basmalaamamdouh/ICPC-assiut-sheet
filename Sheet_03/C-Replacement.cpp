439d64ca342c7b8de6e8aa11dcf1e9d1d5c61ae4
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-28 14:09:52 +0200

    Implement array replacement for positive and negative values
    This program reads a size and an array of integers, replacing positive values with 1 and negative values with 2, then outputs the modified array.

---
+++ Sheet_03/C-Replacement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() 
+{
+    long long size ;
+    cin >> size;
+    vector<long long> arr(size);
+  
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+        
+    }
+    for (int i = 0; i< size; i++)
+    {
+        if (arr[i] > 0)
+        {
+            arr[i] =1;
+        }
+          
+        if (arr[i] < 0)
+        {
+            arr[i] = 2;
+        }
+    }
+    for (int i = 0; i < size; i++)
+    {
+       cout<< arr[i]<<" ";
+ 
+    }
+   
+   
+    
+}
