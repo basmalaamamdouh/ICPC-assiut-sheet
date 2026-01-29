179eb269ceb992f9a8302ebdbb2a2fd7f5c6f327
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-29 16:42:34 +0200

    Create A-Create A New String.cpp

---
+++ Sheet_04/A-Create A New String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include<iomanip>
+using namespace std;
+ 
+int main()
+{
+ 
+string a, b;
+cin >> a >> b;
+cout << a.length() << " " << b.length()<<endl;
+cout <<a+" "+b;
+}
+ 
