9e17ee6634a92266569627833794a2286d09a7d2
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-01-31 22:24:50 +0200

    Implement comparison of two strings

---
+++ Sheet_04/C-Compare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string X, Y;
+    cin >> X >> Y;
+ 
+    if (X <= Y)
+        cout << X;
+    else
+        cout << Y;
+ 
+    return 0;
+}
