43da72a22f72679ad349cbdda4f3d84c53ba8f57
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-02-06 20:30:28 +0200

    Update J-Count Letters.cpp

--- Sheet_04/J-Count Letters.cpp
+++ Sheet_04/J-Count Letters.cpp
@@ -7,11 +7,11 @@ int main() {
     string s;
     cin >> s;
 
-    for (int i = 97; i <= 122; i++) {   // من 'a' لـ 'z'
+    for (int i = 97; i <= 122; i++) {   
         char ch = (char)i;
         int cnt = count(s.begin(), s.end(), ch);
 
-        if (cnt > 0) {   // اطبع بس لو الحرف موجود
+        if (cnt > 0) { 
             cout << ch << " : " << cnt << endl;
         }
     }
