5e39454cc939ef4f74d6e09d4ae8ad0812f32b8c
Basmala Mamdouh <148335386+basmalaamamdouh@users.noreply.github.com>
2026-02-09 14:50:40 +0200

    Create M-Subsequence String.cpp

---
+++ Sheet_04/M-Subsequence String.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    string word = "hello";
+
+    cin >> s;
+
+    int i = 0, j = 0;
+
+    while (i < s.size() && j < word.size()) {
+        if (s[i] == word[j])
+            j++;
+        i++;
+    }
+
+    if (j == word.size())
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
